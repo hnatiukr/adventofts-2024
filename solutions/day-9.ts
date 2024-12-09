d55d25fad6b03479759783e27b9622b5d2a2fed0
Roman Hnatiuk <hnatiukr@pm.me>
2024-12-09 12:42:39 +0100

    resolve: day 9

---
+++ solutions/day-9.ts
@@ -0,0 +1,29 @@
+import type { Equal, Expect } from "type-testing";
+// import type { Child, List, Status } from "santas-special-list";
+
+// declare module "santas-special-list" {
+type Status = "naughty" | "nice";
+
+type Child = {
+  name: string;
+  status: Status;
+};
+
+type List = Child[];
+// }
+
+type t0_actual = Status; // =>
+type t0_expected = "naughty" | "nice"; // =>
+type t0 = Expect<Equal<t0_actual, t0_expected>>;
+
+type t1_actual = Child; // =>
+type t1_expected = {
+  // =>
+  name: string;
+  status: Status;
+};
+type t1 = Expect<Equal<t1_actual, t1_expected>>;
+
+type t2_actual = List; // =>
+type t2_expected = Child[]; // =>
+type t2 = Expect<Equal<t2_actual, t2_expected>>;
